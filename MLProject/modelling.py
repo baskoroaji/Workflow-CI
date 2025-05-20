a956d5752cfb6bee416bb140836e66bb4026f73a
sayoorJengkol <62087838+baskoroaji@users.noreply.github.com>
2025-05-20 23:26:08 +0700

    first commit

---
+++ MLProject/modelling.py
@@ -0,0 +1,41 @@
+import mlflow
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import random
+import numpy as np
+import sys
+
+if __name__ == "__main__":
+    train_path = sys.argv[1]
+    test_path = sys.argv[2]
+
+PATH = "supplement_sales_preprocessed/sales_train_preprocessed.csv"
+data = pd.read_csv(PATH)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    data.drop("Revenue", axis=1), data["Revenue"], test_size=0.2, random_state=42
+)
+input_example = X_train[0:5]
+
+
+with mlflow.start_run():
+    
+    n_estimators = 200
+    max_depth = 20
+    mlflow.autolog()
+    model = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    r2 = model.score(X_test, y_test)
+    mae = mean_absolute_error(y_test, y_pred)
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    mlflow.log_metric("r2_score", r2)
+    mlflow.log_metric("mae", mae)
+    mlflow.log_metric("rmse", rmse)
+    mlflow.sklearn.log_model(
+        sk_model = model,
+        artifact_path="model",
+        input_example=input_example
+    )
